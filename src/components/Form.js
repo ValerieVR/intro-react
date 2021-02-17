4298d65e043e75525109dc312b431f75a64f3e97
Valerie <akpotevalerie@gmail.com>
2021-02-17 19:45:01 +0100

    Remove form tags

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Form = () => {
     return (
-        <form action="" method="">
-            <input type="text" placeholder="Type a new todo"/><br></br>
-            <button type="submit">Add Todo</button>
-        </form>
+        <div>
+            <input type="text" placeholder="Type a new todo"/><br/>
+            <button>Add Todo</button>
+        </div>
     );
 }
 
