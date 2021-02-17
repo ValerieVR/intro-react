25a747c35278e13ecbef4498563d3a2333313339
Valerie <akpotevalerie@gmail.com>
2021-02-17 15:24:55 +0100

    Replace list items by map function

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, {useState} from "react";
 
 const ToDoList = () => {
+    const initialTodos = [
+        {id: 1, name: "Learn HTML", complete: false},
+        {id: 2, name: "Learn CSS", complete: true},
+        {id: 3, name: "Learn JavaScript", complete: false}
+    ];
+    const [todos, setTodos] = useState(initialTodos);
     return (
         <div className="todolist">
             <h3>Todos</h3>
             <ul>
-                <li>
-                    <input type="checkbox"/>Learn HTML<br></br>
-                </li>
-                <li>
-                    <input type="checkbox"/>Learn CSS<br></br>
-                </li>
-                <li>
-                    <input type="checkbox"/>Learn JavaScript
-                </li>
+                {todos.map((todo)=>(
+                    <li key={todo.id}>
+                        <input type="checkbox" checked={todo.complete}/> {todo.name}
+                    </li>
+                ))}
             </ul>
         </div>
     );
